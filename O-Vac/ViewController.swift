7c9ba41ce106eda237fe263cd3a28ac2954f560d
joshgutterman <joshgutterman@eduroam-169-233-201-14.ucsc.edu>
2017-04-23 20:23:33 -0700

    Initial Commit

---
+++ O-Vac/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  O-Vac
+//
+//  Created by Josh Gutterman on 4/23/17.
+//  Copyright © 2017 O-Vac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ O-VacTests/O_VacTests.swift
@@ -0,0 +1,36 @@
+//
+//  O_VacTests.swift
+//  O-VacTests
+//
+//  Created by Josh Gutterman on 4/23/17.
+//  Copyright © 2017 O-Vac. All rights reserved.
+//
+
+import XCTest
+@testable import O_Vac
+
+class O_VacTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
